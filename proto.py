b45e24e348268a10e63bd58328b05d83d8b76543
Evan Chong <evan.tkchong@gmail.com>
2019-11-26 02:50:08 +0800

    trying to come up with an API that makes sense

--- proto.py
+++ proto.py
@@ -1,23 +1,36 @@
 import numpy as np
 import simpleaudio
 
-frequency = 440  # Our played note will be 440 Hz
-fs = 44100  # 44100 samples per second
-seconds = 3  # Note duration of 3 seconds
+frequency = 220  # Our played note will be 440 Hz
+fs = 8000  # 44100 samples per second
+seconds = 0.5  # Note duration of 3 seconds
 
 # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
 t = np.linspace(0, seconds, seconds * fs, False)
 
 # Generate a 440 Hz sine wave
 note = np.sin(frequency * t * 2 * np.pi)
+note = frequency * t * 2 * np.pi
+print(note)
 
 # Ensure that highest value is in 16-bit range
 audio = note * (2**15 - 1) / np.max(np.abs(note))
 # Convert to 16-bit data
-audio = audio.astype(np.int16)
+audio = audio.astype(np.int8)
 
 # Start playback
 play_obj = simpleaudio.play_buffer(audio, 1, 2, fs)
 
 # Wait for playback to finish before exiting
 play_obj.wait_done()
+
+bpm = 120
+intro_melody = 'somefile.txt'
+intro = Melody(intro_melody, bpm)
+intro = 
+
+for i in range(4):
+    intro.play()
+
+    if i == 4:
+        
--- pylodica/core.py
+++ pylodica/core.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+SAMPLING_FREQUENCY = 8000
+
 def tone_frequency(n):
     '''
     Formula for calculating the frequency of the nth key
@@ -7,20 +9,33 @@ def tone_frequency(n):
     return 440 * (((2)**(1/12))**(n-49))
 
 note_dict = {
-    'A♯': tone_frequency(48)
-    'A4': tone_frequency(49),
+    'A♯': tone_frequency(48),
+    'A4': tone_frequency(49)
 }
 
-def note_renderer(frequency):
-    fs = 44100  # 44100 samples per second
-    seconds = 3  # Note duration of 3 seconds
-    # Generate a 440 Hz sine wave
-    note = np.sin(frequency * t * 2 * np.pi)
-    # Ensure that highest value is in 16-bit range
-    audio = note * (2**15 - 1) / np.max(np.abs(note))
-    # Convert to 16-bit data
-    audio = audio.astype(np.int16)
 
-class FunkyBeat(object):
+
+class Song(object):
+    def __init__(self, bpm, wave=np.sin):
+        self._bpm = bpm
+        self._wave = wave
+    
+    def _note_write(frequency):
+        fs = 44100  # 44100 samples per second
+        seconds = 1  # Note duration of 3 seconds
+        # Generate a 440 Hz sine wave
+        note = self._wave(frequency * t * 2 * np.pi)
+        # Ensure that highest value is in 16-bit range
+        audio = note * (2**15 - 1) / np.max(np.abs(note))
+        # Convert to 16-bit data
+        audio = audio.astype(np.int8)
+        return audio
+    
+    def play(self):
+        play_obj = simpleaudio.play_buffer(audio, 1, 2, SAMPLING_FREQUENCY)
+
+class Rythmn(object):
     def __init__(self, bpm):
         self._bpm = bpm
+
+class Melody
